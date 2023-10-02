6ad8eb3eb5e1ef96b1db7d41bbf86e321e4443f7
haris2055 <78054284+haris2055@users.noreply.github.com>
2023-10-02 17:50:58 -0400

    Update Player.h

--- Player.h
+++ Player.h
@@ -6,27 +6,28 @@
 #define COMP345_PLAYER_H
 #include <string>
 
-
 class Player {
     private:
         //Attributes
-        std::string name;
-        static int playerCount;
+        std::string *name;
+
 
     public:
         //Constructor
         Player();
-        Player(std::string name);
+        Player(const std::string &name);
         Player(const Player& other);
 
         //Destructor
         ~Player();
 
         //Methods
-        std::string getName();      //Extra
-        void changeName(std::string newName);
+        std::string getName() const;      //Extra
+        void changeName(const std::string &newName);
         void toAttack();
         void toDefend();
-        void issueOrder(std::string order);
+        void issueOrder(const std::string &order);
+
 };
 #endif //COMP345_PLAYER_H
+
