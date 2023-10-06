979f1197c87ad0478f60fec85eaf184b47f73b30
haris2055 <78054284+haris2055@users.noreply.github.com>
2023-10-06 19:00:14 -0400

    Update PlayerDriver.cpp

--- drivers/PlayerDriver.cpp
+++ drivers/PlayerDriver.cpp
@@ -1,35 +1,51 @@
-//
-// Created by Haris Mahmood on 2023-09-27.
-//
+
 #include <iostream>
+#include "Drivers.h"
 #include "../Player.h"
 
 using namespace std;
 
-void testplayer () {
+void testplayer() {
 
     //Object with pointer created on the heap (Manually have to delete)
-    Player *player1 = new Player("Bob");
+    Player *player = new Player("Bob");
 
-    cout << "Player 1 name is: "<<player1->getName()<<"\n\n";
-    delete player1;
+    std::cout<<"The Player Name is: "<<player->getName()<<"\n";
 
-    Player player2;
-    Player *player3=new Player(player2);
-    cout << "Player 3 name is: "<<player3->getName()<<"\n\n";
-    delete player3;
+    // Issue some orders
+    player->issueOrder("Deploy", 1, 2, 3, 4);
+    player->issueOrder("Advance", 2, 3, 2, 5);
+    player->issueOrder("Bomb", 3, 4, 0, 0);
+    player->issueOrder("Blockade", 4, 5, 0, 0);
+    player->issueOrder("Airlift", 5, 6, 4, 0);
+    player->issueOrder("Negotiate", 7, 0, 0, 8);
 
+    // Check the orders that were given
+    std::cout << "Orders executed:\n";
 
-//    Player *player2 = new Player(*player1);
-//    cout << "Player 2 name is: "<<player2->getName()<<"\n\n";
-//    delete player2;
-//
-//    Player *player3 = new Player("s");
-//    cout <<"Player 3 name is: "<<player3->getName()<<"\n";
-//    delete player3;         // To delete a object
-//
-//    //Object without pointer created on the stack (Automatically deletes)
-//    Player player4;
-//    cout << "Player 4 name is: "<<player4.getName()<<"\n";
+    for (const Order* order : player->getOrderList())
+    {
+        std::cout << *order << std::endl;
+    }
+
+    //---------------------------------
+
+    // Get some territories from the loaded map
+    Territory *territory1 = new Territory();
+    Territory *territory2 = new Territory();
+    Territory *territory3 = new Territory();
+
+    // Associate territories with the player
+    player->addTerritory(*territory1);
+    player->addTerritory(*territory2);
+    player->addTerritory(*territory3);
+
+    // Print the player's territories
+    cout << "Player " << player->getName() << " owns the following territories:\n";
+
+    const std::vector<Territory>* playerTerritories = player->getTerritories();
+    for (const Territory& territory : *playerTerritories) {
+        std::cout << "Territory Name: " << territory.getName() << std::endl;
+    }
 
 };
