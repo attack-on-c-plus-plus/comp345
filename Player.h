d3699d4fc3c587227e069df2de12221111951a15
haris2055 <78054284+haris2055@users.noreply.github.com>
2023-10-06 18:59:04 -0400

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,56 +1,66 @@
-//
-// Created by Haris Mahmood on 2023-09-27.
-//
 
 #ifndef COMP345_PLAYER_H
 #define COMP345_PLAYER_H
-#include <string>
 
-//Temp will need to remove after
-class Map{};
-class Territory{};
-class OrdersList{};
-class Hand{};
-class Deck{};
-class Card{};
-//------------------------
+#include <string>
+#include <vector>
+#include "Cards.h"
+#include "Map.h"
+#include "Orders.h"
+
+// forward declaration
+class Map;
+
+class Territory;
+
+class OrdersList;
 
 class Player {
-    private:
-        //Attributes
-        std::string *name;
-        std::vector<Territory> territories;
-        OrdersList ordersList;
-        Hand hand;
+public:
+    //Constructor
+    Player();
 
-    public:
+    explicit Player(const std::string &name);
 
-        //Constructor
-        Player();
-        Player(const std::string &name);
-        Player(const Player& other);
+    Player(const Player &other);
 
-        //Destructor
-        ~Player();
+    //Destructor
+    ~Player();
 
-        //Methods
-        std::string getName() const;
-        void changeName(const std::string &newName);
+    //Methods
+    [[nodiscard]] std::string &getName() const;
 
-        std::vector<Territory> toAttack();
-        std::vector<Territory> toDefend();
+    void changeName(const std::string &newName);
 
-        void issueOrder(const std::string &orderType, int sourceTerritory, int targetTerritory, int armies, int targetPlayerID);
+    [[nodiscard]] std::vector<Territory> toAttack(const Map &) const;
 
-        // Add methods to manage the player's territory
-        const std::vector<Territory>& getTerritoriesOwned() const;
-        void addTerritory(const Territory &territory);
-        bool removeTerritory(const Territory &territoryToRemove);
+    [[nodiscard]] std::vector<Territory> toDefend(const Map &) const;
 
-        // Add methods to manage the player's hand of cards
-        void drawCardFromDeck(Deck& deck);
-        void playCardFromHand(const Card& card, Deck& deck);
-        const Hand& getHand() const;
+    void issueOrder(const std::string &orderType, int sourceTerritory, int targetTerritory, int armies, int targetPlayerID);
+
+    //method that views the order list of the player
+    std::vector<Order*> getOrderList();
+
+    // Add methods to manage the player's territory
+    void addTerritory(const Territory &territory);
+
+    bool removeTerritory(const Territory &territoryToRemove);
+
+    // Add methods to manage the player's hand of cards
+    void drawCardFromDeck(Deck &deck);
+
+    void playCardFromHand(Card &card, Deck &deck);
+
+    [[nodiscard]] const Hand &getHand() const;
+
+    const std::vector<Territory>* getTerritories() const;
+
+private:
+    //Attributes
+    std::string *name;
+    std::vector<Territory> *territories;
+    OrdersList *ordersList;
+    Hand *hand;
 
 
 };
