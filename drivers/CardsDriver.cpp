ebbb95531f0c0568ce81f21188305945a5e05440
Carson Senthilkumar <77632579+CarsonSSk@users.noreply.github.com>
2023-11-07 20:49:51 -0500

    Update CardsDriver.cpp
    fixed card creation

--- drivers/CardsDriver.cpp
+++ drivers/CardsDriver.cpp
@@ -24,12 +24,14 @@ void testCards() {
     std::cout << "===================================" << std::endl;
 
     // Creating cards
+    
     Card card1{CardType::bomb};
-    Card card2{CardType::advance};
-    Card card3{CardType::deploy};
-    Card card4{CardType::blockade};
-    Card card5{CardType::airlift};
-    Card card6{CardType::diplomacy};
+    Card card2{CardType::reinforcement};
+    Card card3{CardType::blockade};
+    Card card4{CardType::airlift};
+    Card card5{CardType::diplomacy};
+
+    Card card6{CardType::bomb};
 
     std::cout<<std::endl;
     std::cout << "===================================" << std::endl;
