1a8fc3024d8dd4d09a491ec441c787fbdb5ff701
haris2055 <78054284+haris2055@users.noreply.github.com>
2023-12-01 17:26:07 -0500

    Create PlayerStrategiesDriver.cpp

---
+++ drivers/PlayerStrategiesDriver.cpp
@@ -0,0 +1,89 @@
+/**
+ ************************************
+ * COMP 345 Professor Hakim Mellah
+ ************************************
+ * @author Team 5 Attack on C++
+ * @author Daniel Soldera
+ * @author Carson Senthilkumar
+ * @author Joe El-Khoury
+ * @author Henri Stephane Carbon
+ * @author Haris Mahmood
+ */
+
+#include "PlayerStrategiesDriver.h"
+#include "../PlayerStrategies.h"
+#include "../GameEngine.h"
+#include "../Player.h"
+#include "../CommandProcessing.h"
+#include <iostream>
+
+void testPlayerStrategies() {
+    const FakeRandom random;
+    CommandProcessor commandProcessor;
+    GameEngine gameEngine{commandProcessor, random};
+
+    const std::string separator(70, '=');
+
+//------------------------------------------------------------------------------------------
+    // Create a Human player and demonstrate its behavior
+    std::cout << separator << std::endl;
+    std::cout << "Testing Human Player Strategy" << std::endl;
+    std::cout << separator << std::endl;
+
+    Player humanPlayer{gameEngine, "Human", Strategy::Human};
+    HumanPlayerStrategy humanStrategy{humanPlayer, gameEngine};
+
+    // Demonstrate Human player behavior
+    std::cout << "Initial State:" << std::endl;
+    std::cout << humanPlayer << std::endl;
+    humanPlayer.issueOrders();
+    std::cout << "After Issuing Orders:" << std::endl;
+    std::cout << humanPlayer << std::endl;
+//------------------------------------------------------------------------------------------
+    // Create a Benevolent player and demonstrate its behavior
+    std::cout << separator << std::endl;
+    std::cout << "Testing Benevolent Player Strategy" << std::endl;
+    std::cout << separator << std::endl;
+
+    Player benevolentPlayer{gameEngine, "Benevolent", Strategy::Benevolent};
+    BenevolentPlayerStrategy benevolentStrategy{benevolentPlayer, gameEngine};
+
+    // Demonstrate Benevolent player behavior
+    std::cout << "Initial State:" << std::endl;
+    std::cout << benevolentPlayer << std::endl;
+    benevolentPlayer.issueOrders();
+    std::cout << "After Issuing Orders:" << std::endl;
+    std::cout << benevolentPlayer << std::endl;
+//------------------------------------------------------------------------------------------
+    // Create a Neutral player and demonstrate its behavior
+    std::cout << separator << std::endl;
+    std::cout << "Testing Neutral Player Strategy" << std::endl;
+    std::cout << separator << std::endl;
+
+    Player neutralPlayer{gameEngine, "Neutral", Strategy::Neutral};
+    NeutralPlayerStrategy neutralStrategy{neutralPlayer, gameEngine};
+
+    // Demonstrate Neutral player behavior
+    std::cout << "Initial State:" << std::endl;
+    std::cout << neutralPlayer << std::endl;
+    neutralPlayer.issueOrders();  // Neutral player takes a balanced approach
+    std::cout << "After Issuing Orders:" << std::endl;
+    std::cout << neutralPlayer << std::endl;
+//------------------------------------------------------------------------------------------
+    // Create a default Player and demonstrate its behavior
+    std::cout << separator << std::endl;
+    std::cout << "Testing Default Player Strategy" << std::endl;
+    std::cout << separator << std::endl;
+
+//    //Issue: Theres no way of creating a default player????
+//    Player defaultPlayer{gameEngine, "Default"};
+
+//    // Demonstrate Default player behavior
+//    std::cout << "Initial State:" << std::endl;
+//    std::cout << defaultPlayer << std::endl;
+//    defaultPlayer.issueOrders();  // Default player has a basic strategy
+//    std::cout << "After Issuing Orders:" << std::endl;
+//    std::cout << defaultPlayer << std::endl;
+//------------------------------------------------------------------------------------------
+
+}
